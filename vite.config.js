dcbe3f5e916251e962e38d8f1cf0b09bcdd56aca
EmanuelTrindde <emanuel.trindade@sanp.com.br>
2026-01-07 14:27:41 -0300

    atualiza configurações

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Permitir acesso externo
+    host: true, 
     watch: {
-      usePolling: true, // Habilitar polling para detectar alterações
+      usePolling: true, 
     },
   },
 })
